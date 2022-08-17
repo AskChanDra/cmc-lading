957104efbe0344f121db2d41d04fd8bac15fc21e
AskChandra <cpthapa@gmail.com>
2022-08-17 10:06:14 +1000

    Barebone structure of Page added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,28 @@ module.exports = {
     "./*.{html,js}"
   ],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '540px',
+      'md': '720px',
+      'lg': '960px',
+      'xl': '1140px',
+      '2xl': '1320px',
+    },
+
+    container: {
+      center: true,
+    },
+
+    extend: {
+      colors: {
+        lightBlue: '#00deff',
+        darkBlack: '#1b2429',
+        lightGray: '#333',
+        darkGray: '#9095a7',
+        darkRed: ' #d0021b',
+        darkGreen: '#1ab99f'
+      },
+    },
   },
   plugins: [],
 }
---
+++ vit.config.js
@@ -0,0 +1,3 @@
+export default {
+  pulicDir: './assets'
+}
